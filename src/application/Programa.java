a1dc8199ee643301a2f5223e3f6c7e0de32ba878
NitroRiser <jonathanfariastech@gmail.com>
2022-08-03 16:05:13 +0000

    commit

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,11 +1,22 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  * Programa
  */
 public class Programa {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.println("Check-in date (dd/mm/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
     }
 }
