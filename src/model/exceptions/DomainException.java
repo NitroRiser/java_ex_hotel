695cac3af87b9a07b56f5b52c14da9f686d3db2f
NitroRiser <jonathanfariastech@gmail.com>
2022-08-03 23:48:09 -0300

    good solution

--- src/model/exceptions/DomainException.java
+++ src/model/exceptions/DomainException.java
@@ -1,9 +1,7 @@
 package model.exceptions;
 
-import java.io.Serial;
-
 public class DomainException extends RuntimeException {
-    public DomainException(String msg){
+    public DomainException(String msg) {
         super(msg);
     }
 }
